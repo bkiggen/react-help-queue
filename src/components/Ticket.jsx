6a13cfcada1ac37f3e923f4b9a43310a38f08044
kiggen <32989298+kiggen@users.noreply.github.com>
2018-11-04 15:18:11 -0800

    finish tutorial

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Ticket(props){
+  var TicketStyles = {
+    backgroundColor: '#ecf0f1',
+    fontFamily: 'sans-serif',
+    paddingTop: '50px'
+  }
+  var issueStyle = {
+    color: 'blue',
+    fontSize: '33px'
+  }
+
   return (
-    <div>
+    <div style={TicketStyles}>
       <h3>{props.location} - {props.names}</h3>
-      <p><em>{props.issue}</em></p>
+      <p style={issueStyle}><em>{props.issue}</em></p>
       <hr/>
     </div>
   );
